4d2f07d64fe9e896f718fef36ade5aca14875036
mmacdonald1 <melmacd94@gmail.com>
2018-12-12 16:12:29 -0500

    cors and serializer

---
+++ app/serializers/card_serializer.rb
@@ -0,0 +1,3 @@
+class CardSerializer < ActiveModel::Serializer
+  attributes :id, :title, :content
+end
---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,4 @@
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :username, :email, :password_digest
+  has_many :decks
+end
