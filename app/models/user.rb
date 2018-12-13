a4948b842a1894c2e415e8f86f43f047f6628afc
mmacdonald1 <melmacd94@gmail.com>
2018-12-13 10:37:06 -0500

    bcrypt has secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+
   validates :username, presence: true, uniqueness:true
   validates :email, presence: true, uniqueness:true
   validates :password_digest, presence: true
+
   has_many :decks
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
+  has_secure_password
   attributes :id, :username, :email, :password_digest
   has_many :decks
 end
