d63ac4d98c06b416eb445128610d8721943e54fa
mmacdonald1 <melmacd94@gmail.com>
2018-12-13 23:33:55 -0500

    auth done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::API
 
+  #secret key
+  def s_key
+    'h3llo'
+  end
+
+  #given a payload return a token
+  def encode(payload)
+    token = JWT.encode(payload, s_key, 'HS256')
+  end
+
+  #given a token return the original payload
+  def decode(token)
+    JWT.decode(token, s_key, true, {algorithm:'HS256'})[0]
+  end
+  
 end
--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,14 +1,23 @@
 class AuthController < ApplicationController
-  def create # POST /api/v1/login
-    byebug
+  def create # POST /login
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
+      payload = {user: @user.id}
+      token = encode(payload)
+      #send back a JWT token
       render json: {
         message: 'correct username and password',
+        token: token,
+        error: false,
+        user: {
+          username: @user.username,
+          email: @user.email
+        }
         }, status: :accepted
     else
       render json: {
         message: 'incorrect username or password',
+        error: true
         }, status: :unauthorized
     end
   end
