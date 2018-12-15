c41406aaf8e78b11b3e4d23fc012a9076a2b9908
mmacdonald1 <melmacd94@gmail.com>
2018-12-14 19:19:20 -0500

    deck crud

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,5 +1,34 @@
 class DecksController < ApplicationController
+  before_action :find_deck, only [:edit, :update]
+
   def index
    render json: Deck.all
- end
+  end
+  def show
+    render json: Deck.find(params[:id])
+  end
+  def new
+    @deck = Deck.new
+  end
+  def create
+    @deck = Deck.create(deck_params)
+  end
+  def edit
+  end
+  def update
+    @deck.update(user_params)
+  end
+  def destroy
+    Deck.find(params[:id]).destroy
+  end
+
+  private
+
+  def find_deck
+    @deck = Deck.find(params[:id])
+  end
+  def deck_params
+    params.require(:deck).permit(:name)
+  end
+
 end
