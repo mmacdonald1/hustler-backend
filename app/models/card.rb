4744331445414f84d1d4462f74c7bb09a39b6317
mmacdonald1 <melmacd94@gmail.com>
2018-12-17 16:54:13 -0500

    fix association

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,4 @@
 class Card < ApplicationRecord
   validates :title, presence: true
+  belongs_to :deck
 end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,4 +1,5 @@
 class Deck < ApplicationRecord
   validates :name, presence: true
   has_many :cards
+  belongs_to :user
 end
