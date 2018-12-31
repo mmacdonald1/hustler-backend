50e4f7d6b75598dc131169ef29736ff817e8e346
mmacdonald1 <melmacd94@gmail.com>
2018-12-31 16:39:59 -0500

    returns cards based on deck id

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,6 +1,9 @@
 class CardsController < ApplicationController
   def index
-   @cards = Card.where(params[:id] == :deck_id)
+   @cards = Card.where(deck_id:params[:id])
+   puts params[:id]
+   puts :deck_id
+   puts @cards
    render json: {cards: @cards}
   end
 end
